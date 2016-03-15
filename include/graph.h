6f7003ca64f77005b2a638ecde40e0d7a09f3367
Colin Stevens <colin@colinjstevens.com>
2016-03-14 20:32:24 -0500

    queue sigs

--- include/graph.h
+++ include/graph.h
@@ -18,9 +18,23 @@ typedef struct graph { // Struct containing all adjacency lists as an array
     adjList* lists;
 } graph;
 
+typedef struct vistedNode {
+    int nodeVal;
+    int dist;
+} visitedNode;
+
+typedef struct queue {
+    visitedNode* head;
+    visitedNode* tail;
+} queue;
+
 listNode* makeNode(int); // Creates a new listNode with dst value `dst` and returns a listNode struct
 graph* createGraph(int size); // Creates a new undirected graph, with `size` linked lists
 void createEdge(graph* graph, int src, int dst); // Adds an edge to a graph struct
+void printDistancesFromOrigin(graph* graph); // Returns shortest distance from `dst` to vertex 1
+void createQueue;
+void enqueue(queue, visitedNode*);
+visitedNode* dequeue(queue);
 
 //DEBUG
 void printGraph(graph* graph);
