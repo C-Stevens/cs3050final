16f9f9773df7eab4ad8641d81052b79ba9c53d83
Colin Stevens <colin@colinjstevens.com>
2016-05-04 21:10:41 -0500

    added signatures

--- include/trade.h
+++ include/trade.h
@@ -3,16 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <file_io.h>
 
 typedef struct trade {
-    int day1;
-    int day2;
+    int buyDay;
+    int sellDay;
 } trade;
 
 typedef struct tradeScheme {
     int potential;
     int tradeListSize;
-    struct trade* trades;
+    trade* trades;
 } tradeScheme;
+
+tradeScheme* trade(int r, int* prices); // trade(r, prices)
+tradeScheme* tryTrade(int day1, int day2, int r); // tryTrade(day1, day2, r)
 
 #endif
